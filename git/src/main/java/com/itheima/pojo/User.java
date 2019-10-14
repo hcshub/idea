262abd44c5dcdc8f9c196ca5cdd7975aabe4271a
Administrator <18372568672@139.com>
2019-10-14 11:09:36 +0800

    第一次

---
+++ git/src/main/java/com/itheima/pojo/User.java
@@ -0,0 +1,14 @@
+package com.itheima.pojo;
+
+/*
+ *@ProjectName: GIt
+ *@Package: com.itheima.pojo
+ *@ClassName: User
+
+ *@author HCS
+ *@CreateData 2019-10-14 11:08
+ *@UpdateData 2019-10-14 11:08
+ */
+public class User {
+    private  String user;
+}
