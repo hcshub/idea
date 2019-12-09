fc9d8a203b82f6068c9803658f7dda7fa0029c5b
Administrator <18372568672@139.com>
2019-12-09 11:17:24 +0800

    12/34

---
+++ git/src/main/java/com/itheima/pojo/Dem.java
@@ -0,0 +1,31 @@
+package com.itheima.pojo;
+
+/*
+ *@ProjectName: GIt
+ *@Package: com.itheima.pojo
+ *@ClassName: Dem
+
+ *@author HCS
+ *@CreateData 2019-12-09 11:13
+ *@UpdateData 2019-12-09 11:13
+ */
+public class Dem {
+   private String name;
+    private Integer age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
